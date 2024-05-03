ecf7ea27c3ef51742b18cc8b0f97f4fa1bad866a
FIRST_NAME LAST_NAME <ashikjoseph17@gmail.com>
2024-05-03 17:33:31 +0530

    changed

--- devend/controllers/chatController.js
+++ devend/controllers/chatController.js
@@ -1,7 +1,8 @@
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken')
 
-const User = require('/ChatApp/devend/models/userSchema')
+const User = require('../../../models/userSchema')
+
 
 exports.signUp = async (req, res) => {
     // res.status(200).json('Signup user')
